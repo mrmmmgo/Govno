21313add36cbb189471a51f663910ef37f393948
Mr_MMM_go <mrmmmgo@gmail.com>
2023-08-18 11:26:51 +0500

    add files

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-alert('BooM');
-document.write('BooM');
-console.log('LoX');
-confirm('IdI nA XuY');
-prompt('PoShOl tI v JoPu');
-+prompt('PoShOl tI v XuY');
+// alert('BooM');
+// document.write('BooM');
+// console.log('LoX');
+// confirm('IdI nA XuY');
+// prompt('PoShOl tI v JoPu');
+// +prompt('PoShOl tI v XuY');
+
+// let mane = prompt("Am PrOgRaMmEr hEeHe");
+// let age = +prompt("SkOlKo TeBe Let?");
+// let adres = prompt("GdE Ti JivEsH?");
+
+// document.write(`NaMe ${mane} . GoD RoJdEnIyA ${2023-age} . ${adres}-DoM`)
+
+// function hello (name) {
+//     document.write("IdI Na XuY" + mane)
+// }
+
+// hello('NoT gAy');
+
+let num = +prompt('VvEdiI ChIsLo');
+
+if (num == 1) {
+    window.location = 'https://project1original.netlify.app/'
+} if (num == 2) {
+    window.location = 'https://windiws11.mrmmmgo.netlify.app/'
+} if (num == 3) {
+    window.location = 'https://windiws10mrmmmgo.netlify.app/'
+}
